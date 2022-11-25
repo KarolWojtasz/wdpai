4e201b28e1fe2a8447dc95100bfa46b8ada0595b
PonuryZniwiarz2k <karolwojtasz@gmail.com>
2022-11-25 19:34:05 +0100

    added router basics and fixed docker folder

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php 
-echo "Hello world";
+require "Router.php";
+
+$path = trim($_SERVER['REQUEST_URI'],'/');
+$path = parse_url($path,PHP_URL_PATH);
+
+Router::get("index","DefaultController");
+Router::run($path);
 ?>
